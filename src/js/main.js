629926c19a9940461de61010db69221d0bb9aabf
Will King <wking@Wills-MBP.knology.net>
2017-05-21 08:31:54 -0500

    Initial commit.

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import '../scss/main.scss';
+
+/* Test To Make Sure It Works */
+const root = document.querySelector('.site-description');
+root.innerHTML = '<pre>var hello = world;</pre>';
+console.log('hello');
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+// Requires
+const webpack = require('webpack')
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+// Plugin Config
+const extractCommons = new webpack.optimize.CommonsChunkPlugin({
+  name: 'commons',
+  filename: 'commons.js'
+})
+const extractCSS = new ExtractTextPlugin('[name].bundle.css')
+
+const config = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    main: './js/main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      enforce: 'pre',
+      loader: 'eslint-loader',
+      options: {
+        emitWarnings: true
+      }
+    }, {
+      test: /\.js$/,
+      include: path.resolve(__dirname, 'src/js'),
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['es2015', { modules: false }]
+          ]
+        }
+      }]
+    }, {
+      test: /\.scss$/,
+      include: path.resolve(__dirname, 'src/scss'),
+      loader: extractCSS.extract(['css-loader', 'sass-loader'])
+    }, {
+      test: /\.(jpg|png)$/,
+      use: [{
+        loader: 'url-loader',
+        options: { limit: 10000 }
+      }]
+    }]
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    extractCommons,
+    extractCSS
+  ]
+}
+
+module.exports = config
